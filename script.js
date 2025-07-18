4ee3cb77dfe4b75a81a456a6de69b3b5fa1e9654
Bhuwan Bhatt <ogbhuwanbhatt@gmail.com>
2025-07-18 19:40:36 +0545

    uo

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let x = window.innerWidth / 2;
 let y = window.innerHeight / 2;
 
 // Direction & speed (slow)
-let dx = 0.00008;
-let dy = 0.00008;
+let dx = 0.000000000000008;
+let dy = 0.000000000000008;
 
 function animate() {
   const rect = welcome.getBoundingClientRect();
