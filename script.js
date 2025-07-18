1c539fc07393406e254997d4cbb6f1a97631a8a5
Bhuwan Bhatt <ogbhuwanbhatt@gmail.com>
2025-07-18 19:27:08 +0545

    update

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const welcome = document.getElementById("welcome");
 
 let x = Math.random() * window.innerWidth;
 let y = Math.random() * window.innerHeight;
-let dx = 0.5;
-let dy = 0.5;
+let dx = 0.2;
+let dy = 0.2;
 
 function animate() {
   const rect = welcome.getBoundingClientRect();
